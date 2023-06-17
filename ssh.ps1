54a80b8a94fc3e98d76997aac4688a0222eed816
Criticalminds0x21 <Criticalminds0x21@protonmail.ch>
2023-06-17 12:07:52 +0530

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,14 +1,9 @@
-# Set the path to the check_openssh.ps1 script
 $checkOpenSSHScriptPath = "$PWD\check_openssh.ps1"
-
-# Set the path to the install_openssh.ps1 script
 $installOpenSSHScriptPath = "$PWD\install_openssh.ps1"
-
 $GetInfo = "$PWD\get_info.ps1"
 $replacedata = "$PWD\replace.ps1"
 $wipe = "$PWD\wipeout.ps1"
 
-# Check if OpenSSH is installed
 Write-Host "Checking if OpenSSH is installed..."
 & $checkOpenSSHScriptPath
 $isOpenSSHInstalled = $LASTEXITCODE
@@ -19,9 +14,7 @@ if ($isOpenSSHInstalled -eq 0) {
     & $replacedata
     & $GetInfo
     & $wipe
-}
-else {
+} else {
     Write-Host "OpenSSH is not installed. Installing..."
     & $installOpenSSHScriptPath
-
 }
